42a9b8515871dc7c7cffa1ac3bf2e7b2bca93f22
Kira Mueller <50597894+Sapphiery@users.noreply.github.com>
2024-09-26 21:39:59 -0700

    solve exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    let splitString = inputString.split('');
+    let reversedArray = splitString.reverse();
+    let reversedString = reversedArray.join('');
 
+    return reversedString;
 };
 
 // Do not edit below this line
