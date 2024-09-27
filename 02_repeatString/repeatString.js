def020a52cf93bb6ee9425b4644adcdc295bff36
Kira Mueller <50597894+Sapphiery@users.noreply.github.com>
2024-09-26 21:12:09 -0700

    solve exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, reps) {
+    let repeated = '';
 
+    if (reps < 0)
+        return 'ERROR';
+
+    for (let i = 0; i < reps; i++) {
+        repeated += string;
+    }
+
+    return repeated;
 };
 
 // Do not edit below this line
