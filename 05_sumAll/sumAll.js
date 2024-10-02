31894718dcdb38b9c2a360a0ecd6e1525ace7b5a
Kira Mueller <50597894+Sapphiery@users.noreply.github.com>
2024-10-01 20:00:57 -0700

    solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    let sum = 0;
 
+    let sumFrom, sumTo;
+
+    if ((typeof numOne) != 'number' || (typeof numTwo) != 'number') {
+        return 'ERROR';
+    }
+
+    if ((numOne % 1) != 0 || (numTwo % 1) != 0) {
+        return 'ERROR';
+    }
+
+    if (numOne < 0 || numTwo < 0) {
+        return 'ERROR';
+    }
+
+    if (numOne < numTwo) {
+        sumFrom = numOne;
+        sumTo = numTwo;
+    } else {
+        sumFrom = numTwo;
+        sumTo = numOne;
+    }
+
+
+    for (let i = sumFrom; i <= sumTo; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
