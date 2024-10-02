b5b0490283841f29af6dba96c0824081d2b930bf
Kira Mueller <50597894+Sapphiery@users.noreply.github.com>
2024-10-01 18:40:04 -0700

    solve exercise 4 twice

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(targetArray, ...toRemove) {
+    // const newArray = [];
+
+    // for (const num of targetArray) {
+    //     const needsRemoval = toRemove.find((element) => element === num);
+
+    //     if (!needsRemoval && needsRemoval != 0) {
+    //         newArray.push(num);
+    //     }
+    // }
+
+    // return newArray;
+
+    for (let i = 0; i < toRemove.length; i++) {
+        let index = targetArray.indexOf(toRemove[i]);
+
+        while (index >= 0) {
+            targetArray.splice(index, 1);
+            index = targetArray.indexOf(toRemove[i]);
+        }
+    }
+
+    return targetArray;
 };
 
 // Do not edit below this line
